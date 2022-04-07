eecbec95af59b69308a85fadc620827ff579ab0a
crayon <lsh328328@naver.com>
2022-04-07 22:56:31 +0900

    Feat: 회원가입 및 로그인 Response 객체 수정 Related to: #5

--- src/main/java/com/moodstation/springboot/controller/UserApiController.java
+++ src/main/java/com/moodstation/springboot/controller/UserApiController.java
@@ -1,5 +1,6 @@
 package com.moodstation.springboot.controller;
 
+import com.moodstation.springboot.dto.Message;
 import com.moodstation.springboot.dto.request.CheckEmailDto;
 import com.moodstation.springboot.dto.request.CheckNicknameDto;
 import com.moodstation.springboot.dto.request.LoginDto;
@@ -7,16 +8,21 @@ import com.moodstation.springboot.dto.response.LoginResponse;
 import com.moodstation.springboot.dto.response.UserData;
 import com.moodstation.springboot.dto.request.SignupDto;
 import com.moodstation.springboot.enums.ErrorCode;
+import com.moodstation.springboot.enums.StatusEnum;
 import com.moodstation.springboot.exception.ConflictExecption;
 import com.moodstation.springboot.service.AuthService;
 import com.moodstation.springboot.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.nio.charset.Charset;
 
 @RequiredArgsConstructor
 @RestController
@@ -27,9 +33,19 @@ public class UserApiController {
 
     // 회원가입
     @PostMapping("/signup")
-    public Long signup(@RequestBody @Valid SignupDto signupDto) {
+    public ResponseEntity<Message> signup(@RequestBody @Valid SignupDto signupDto) {
         UserData body = authService.signup(signupDto);
-        return body.getId();
+
+        Message message = new Message();
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+
+        message.setStatus(StatusEnum.OK);
+        message.setMessage("회원가입 성공");
+        message.setData(body);
+
+        return new ResponseEntity<>(message, headers, HttpStatus.OK);
     }
 
     // 회원가입 이메일 중복 체크
@@ -53,8 +69,17 @@ public class UserApiController {
 
     // 로그인
     @PostMapping("/login")
-    public LoginResponse login(@RequestBody LoginDto loginDto) {
+    public ResponseEntity<Message> login(@RequestBody LoginDto loginDto) {
         LoginResponse body = authService.login(loginDto);
-        return body;
+
+        Message message = new Message();
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+
+        message.setStatus(StatusEnum.OK);
+        message.setMessage("로그인 성공");
+        message.setData(body);
+        return new ResponseEntity<>(message, headers, HttpStatus.OK);
     }
 }
--- src/main/java/com/moodstation/springboot/jwt/JwtGenerator.java
+++ src/main/java/com/moodstation/springboot/jwt/JwtGenerator.java
@@ -106,7 +106,7 @@ public class JwtGenerator {
 
     // Request의 Header에서 token 값을 가져옵니다. "ACCESS-TOKEN" : 토큰값
     public String resolveToken(HttpServletRequest request) {
-        return request.getHeader("ACCESS-TOKEN");
+        return request.getHeader("accessToken");
     }
 
     // 토큰의 유효성 + 만료일자 확인
